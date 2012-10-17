6969051897ab8d70fdf8d15ebb900d3356a4d5cd
Ash <amulekar@gmail.com>
2012-10-16 22:25:27 -0700

    photosviewcontroller added end

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -18,7 +18,7 @@ class PhotosController < ApplicationController
        
         #@photos = Photo.where("lat BETWEEN ? AND ?", lat - COORDINATE_DELTA, lat + COORDINATE_DELTA).where("lng BETWEEN ? AND ?", lng - COORDINATE_DELTA, lng + COORDINATE_DELTA).limit(64)
         #respond_with({:message => "Invalid or missing lat/lng parameters"}, :status => 406)
-      #end
+      end
 
     #@photos = Photo.all
     
